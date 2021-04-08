b284cdeabe3ae4e616b9d695be8aa645868d7d6d
Aaron Barlow <abarlow.501@gmail.com>
2021-04-07 21:01:13 -0500

    Began Project. Added Silencer button on initial View controller.

---
+++ TheSilencer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheSilencer
+//
+//  Created by Aaron Barlow on 4/7/21.
+//  Copyright © 2021 Aaron Barlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
